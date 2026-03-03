919c02aa8c5095153c88052c679675d7393ba41f
JPabloMF <juanpablo45671@gmail.com>
2026-03-03 12:07:17 -0500

    Installing primevue

--- spa/src/main.js
+++ spa/src/main.js
@@ -3,11 +3,23 @@ import { createPinia } from "pinia";
 import router from "./router";
 import './assets/styles/main.scss'
 import App from './App.vue'
+// PrimeVue Imports
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: "system",
+    },
+  },
+});
 
 app.mount('#app');
