1c94eb64d552bd5cedbcde76be875949d808f665
JPabloMF <juanpablo45671@gmail.com>
2026-03-31 10:18:05 -0500

    feat: remove authentication requirement for fetching incomes

--- services/src/routes/incomeRoutes.js
+++ services/src/routes/incomeRoutes.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getIncomes);
+router.get("/", getIncomes);
 router.post("/", verifyToken, createIncome);
 router.put("/:id", verifyToken, updateIncome);
 
